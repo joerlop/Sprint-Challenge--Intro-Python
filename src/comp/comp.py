3bc1ea3f5a1a088b0af1c47d6c846be658c59128
Geraldine Lustgarten <geraldinelustgarten@Geraldines-MacBook.local>
2019-09-06 11:01:03 -0500

    Wrote list comprehension for names between C and G

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -36,7 +36,7 @@ print(b)
 # Write a list comprehension that creates a list of names of everyone
 # whose name starts with any letter between 'C' and 'G' inclusive.
 print("Starts between C and G, inclusive:")
-c = []
+c = [h.name for h in humans if h.name[0] == "C" or h.name[0] == "D" or h.name[0] == "E" or h.name[0] == "F" or h.name[0] == "G"]
 print(c)
 
 # Write a list comprehension that creates a list of all the ages plus 10.
